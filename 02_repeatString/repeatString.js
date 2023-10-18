30394b91be3b9ebbb72d342af7122fd073bf1b5a
Alexander Riedl <65710822+SaltyC00kie@users.noreply.github.com>
2023-10-18 23:18:23 +0200

    js Tasks 1 -3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,23 @@
-const repeatString = function() {
+const repeatString = function(pString, pNumber)
+{
+    let outputString = "";
+    if(pNumber < 0)
+    {
+        return "ERROR";
+    }
 
+    if(pString === "")
+    {
+        return "";
+    }
+
+
+    for(let i = 0; i < pNumber; i++)
+    {
+        outputString += pString;
+    }
+    
+    return outputString;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(pString) 
+{
+    return pString.split("").reverse().join("");
 };
 
 // Do not edit below this line
