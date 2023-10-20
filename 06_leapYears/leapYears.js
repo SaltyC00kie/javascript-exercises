4c23e14410b858013d7264c7c3395ec77e4c2e23
Alexander Riedl <65710822+SaltyC00kie@users.noreply.github.com>
2023-10-20 16:44:28 +0200

    task 6 & 7

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,7 @@
-const leapYears = function() {
-
+const leapYears = function(pYear) 
+{
+    // Leap years are years divisible by four (like 1984 and 2004). However, years divisible by 100 are not leap years (such as 1800 and 1900) unless they are divisible by 400 (like 1600 and 2000, which were in fact leap years)
+    return (pYear % 4 === 0 && (pYear % 100 !==  0 || pYear % 400 === 0));
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,10 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(pTemperature) {
+
+  return Math.round((pTemperature - 32 ) * (5/9) * 10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(pTemperature) {
+  return  Math.round(((pTemperature * 1.8)  + 32) * 10) /10;
 };
 
 // Do not edit below this line
